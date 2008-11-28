d793ef671a1bfa6a6a3c3712f10cdb64472193bb
Giulio Paci <giuliopaci@gmail.com>
2008-11-28 12:19:16 +0000

    Added spro from svn://scm.gforge.inria.fr/svn/spro/trunk, revision 142 (2008-03-10 11:32:00 +0100).
    git-svn-id: file:///giuliopaci/svn/spro/trunk/spro-4.0.2@166 af33e3f0-49a3-4c7e-a496-edfa7df5f06c

---
+++ autogen.sh
@@ -0,0 +1,14 @@
+#! /bin/sh
+#
+# $RCSfile$
+# $Author: ggravier $
+# $Date: 2002-07-25 16:14:32 +0200(gio, 25 lug 2002) $
+# $Revision: 61 $
+#
+# Generate all necessary files by running automake, autoconf, ...
+#
+
+aclocal
+autoheader
+automake -a
+autoconf
